aa3bbc40cf8ea20a4ef05248580247649424e0bd
clara-cdp <clarianne.cdp@gmail.com>
2026-06-21 10:35:36 +0200

    Header created

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 export default function App() {
-    return <div>
+    return <div className="max-w-2xl mx-auto p-4 flex flex-col gap-4">
       <Header></Header>
   </div>
 }
 
 function Header() {
-    return<h1>Reactivity</h1>
+    return (
+        <header className="flex items-center justify-between">
+            <div className="flex flex-col gap-1">
+                <h1 className="text-3xl font-bold uppercase">Reactivity</h1>
+                <span className="text-zinc-400 text-sm">1/1 done today</span>
+            </div>
+            <div className="flex flex-col gap-1">
+                <span className="text-zinc-400 text-sm">April- 6 - April 21</span>
+                <div className="flex items-center gap-3">
+                    <button>Prev</button>
+                    <button>Next</button>
+                </div>
+                    
+            </div>
+    </header>
+)
 }
