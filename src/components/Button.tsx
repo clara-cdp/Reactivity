8036261f2342f6539e9f254f31e168470d431992
clara-cdp <clarianne.cdp@gmail.com>
2026-07-01 23:41:42 +0200

    created a HabitList component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,7 +2,6 @@ type ButtonProps = {
     children : ReactNode
 }
 
-
 export function Button({children}: ButtonProps) {
     
     return <button className="bg-violet-600 hover:bg-violet-500 transition-colors rounded px-2 py-1 
---
+++ src/components/HabitList.tsx
@@ -0,0 +1,13 @@
+import {Button} from "./Button"
+
+export function HabitList() {
+    const habits = []
+   
+    if (habits.length === 0) {
+        return (
+            < p className="text-center text-zinc-500 py-12" > No habits yet. Add a new habit above to get started</ p>     
+        )
+    }
+
+   return <h1>full</h1>
+}
