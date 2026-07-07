5039c1e31e8f942ede3486971c406fe3cc895c4b
clara-cdp <clarianne.cdp@gmail.com>
2026-07-08 00:01:39 +0200

    added ComponentProps to Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,15 @@
+import type { ComponentProps } from "react"
+
 type ButtonProps = {
     children: ReactNode
-    disabled?: boolean
-}
+} & ComponentProps<"button">
 
-export function Button({children, disabled = false}: ButtonProps) {
-    
-    return <button disabled={disabled} className="bg-violet-600 hover:bg-violet-500 transition-colors rounded px-2 py-1 
-    disabled:opacity-30 disabbled:cursor-not-allowed">{children}</button>
+export function Button({children, ...props}: ButtonProps) {
+    return (
+        <button {...props} className="bg-violet-600 hover:bg-violet-500 transition-colors rounded px-2 py-1 
+        disabled:opacity-30 disabbled:cursor-not-allowed"
+        >
+            {children}
+        </button>
+    )
 }
