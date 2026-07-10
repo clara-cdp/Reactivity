ccf240811a1a49a2f1f94ee5382fb7c265ac665d
clara-cdp <clarianne.cdp@gmail.com>
2026-07-10 17:48:14 +0200

    added variants to Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,15 +1,28 @@
 import type { ComponentProps } from "react"
 
 type ButtonProps = {
-    children: ReactNode
+    variant?: Variant
 } & ComponentProps<"button">
 
-export function Button({children, ...props}: ButtonProps) {
+export function Button({variant = "primary", ...props}: ButtonProps) {
     return (
-        <button {...props} className="bg-violet-600 hover:bg-violet-500 transition-colors rounded px-2 py-1 
-        disabled:opacity-30 disabbled:cursor-not-allowed"
-        >
-            {children}
-        </button>
+        <button
+            {...props}
+            className={`${getVariantStyles(variant)}
+            transition-colors rounded px-2 py-1 disabled:opacity-30 disabbled:cursor-not-allowed`}
+        />
     )
+}
+
+function getVariantStyles(variant: Variant) {
+    switch (variant) {
+        case "primary":
+            return "bg-teal-600 hover:bg-teal-400"
+        case "secondary":
+            return "bg-zinc-700 hover:bg-zinc-600 text-zinc-400"
+        case "delete":
+            return "hover:bg-red-800 text-red-800 hover:text-red-200"
+        default:
+            throw new Error(`invalid variantÑ $^variant satisfies never}`)
+    }
 }
