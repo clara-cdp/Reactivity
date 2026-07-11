0e930dd6c72471d0d545377d692028109c2a168f
clara-cdp <clarianne.cdp@gmail.com>
2026-07-11 13:01:45 +0200

    created handleSubmit function

--- src/components/Habitform.tsx
+++ src/components/Habitform.tsx
@@ -3,6 +3,16 @@ import { Button } from "./Button"
 
 export function HabitForm() {
     const [name, setName] = useState("");
+
+    function handleSubmit(e: submitEvent) {
+        e.preventDefault()
+        
+        if (name.trim() === "") return
+        setName("")
+
+        console.log(name)
+}
+
     return(
         <form className="flex gap-2">
             <input
@@ -11,7 +21,9 @@ export function HabitForm() {
                 className="flex-1 rounded-lg bg-zinc-800 px-4 py-2 
             outline-none focus-visible:ring-2 focus-visible:ring-violet-500"
             placeholder="New habit"/>
-            <Button className="bg-violet-500 rounded-xl px-4 py-2 font-bold tracking-wide">Add Habit</Button>
+            <Button
+                disabled = {name.trim()===""}
+                className="bg-violet-500 rounded-xl px-4 py-2 font-bold tracking-wide">Add Habit</Button>
         </form>
     )
 }
