83ce6aa67659eee6569b279f10ea892901025dfb
clara-cdp <clarianne.cdp@gmail.com>
2026-07-15 00:05:22 +0200

    show current week - replaced mock dates

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,18 @@
 import { Button } from "./Button"
 import { useHabits } from "../context/useHabits"
-import { isToday } from "date-fns"
+import { isToday, format } from "date-fns"
 
-export function Header() {
+type HeaderProps = {
+    visibleDates: Date[]
+}
+
+export function Header({ visibleDates }: HeaderProps) {
 
     const { habits } = useHabits()
     const doneToday = habits.filter(h =>
         h.completions.some(c => isToday(c)),
     ).length
+    const dateRange = `${format(visibleDates[0], "MMM d")} - ${format(visibleDates.at(-1)!, "MMM d")}`
 
     return (
         <header className="flex items-center justify-between">
@@ -16,7 +21,7 @@ export function Header() {
                 <span className="text-zinc-400 text-sm uppercase">{doneToday} / {habits.length} done today</span>
             </div>
             <div className="flex flex-col gap-1 items-end">
-                <span className="text-zinc-400 text-sm">April- 6 - April 21</span>
+                <span className="text-zinc-400 text-sm">{dateRange}</span>
                 <div className="flex items-center gap-3">
                     <Button>Prev</Button>
                     <Button>Next</Button>
