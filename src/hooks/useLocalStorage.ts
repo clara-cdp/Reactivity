fe612adb7a7e3d5ef1a83cece145ecfbbf5c3911
clara-cdp <clarianne.cdp@gmail.com>
2026-07-15 18:05:33 +0200

    create dateReviver function

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,5 +1,5 @@
-import { parseISO } from "date-fns"
 import { useEffect, useState } from "react"
+import { parseISO } from "date-fns"
 
 export function useLocalStorage<T>(key: string, initialValue: T) {
     const [storedValue, setStoredValue] = useState<T>(() => {
@@ -7,7 +7,7 @@ export function useLocalStorage<T>(key: string, initialValue: T) {
             const item = localStorage.getItem(key)
             if (item == null) return initialValue
             
-            return JSON.parse(item)
+            return JSON.parse(item, dateReviver)
         } catch {
             return initialValue
         }
@@ -19,4 +19,12 @@ export function useLocalStorage<T>(key: string, initialValue: T) {
   }, [storedValue, key])
     
     return [storedValue, setStoredValue] as const
+}
+
+function dateReviver(_key: string, value: unknown) {
+  if (typeof value === "string" && /^\d{4}-\d{2}-\d{2}T/.test(value)) {
+    return parseISO(value)
+  }
+
+  return value
 }
